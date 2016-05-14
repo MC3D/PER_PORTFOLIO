d795c04ec0844a75eca541b9c4a337495014697d
MC3D <m.chapman.home@gmail.com>
2016-05-14 09:07:34 -0400

    adds header links

--- scripts/main.js
+++ scripts/main.js
@@ -8,18 +8,43 @@
 //   }));
 
 
-$(function () {
+$(function() {
   'use strict';
-    var y = $('#nav').offset().top;
-    console.log(y);
+  var x = $('#nav').height() * 2;
+  var y = $('#nav').offset().top;
+  console.log(y);
 
-    $(window).on('scroll', function () {
-        if (y <= $(window).scrollTop()) {
-            // if so, add the fixed class
-            $('#nav').addClass('fixed');
-        } else {
-            // otherwise remove it
-            $('#nav').removeClass('fixed');
-        }
-    });
+  $(window).on('scroll', function() {
+    if (y <= $(window).scrollTop()) {
+      // if so, add the fixed class
+      $('#nav').addClass('fixed');
+    } else {
+      // otherwise remove it
+      $('#nav').removeClass('fixed');
+    }
+  });
+
+  $('#nav-link-skills').on('click', function() {
+    $('html, body').animate({
+      scrollTop: ($('#section-skills').offset().top - x)
+    }, 500); // 500 milliseconds
+  });
+
+  $('#nav-link-projects').on('click', function() {
+    $('html, body').animate({
+      scrollTop: ($('#section-projects').offset().top - x)
+    }, 500); // 500 milliseconds
+  });
+
+  $('#nav-link-timeline').on('click', function() {
+    $('html, body').animate({
+      scrollTop: ($('#section-timeline').offset().top - x)
+    }, 500); // 500 milliseconds
+  });
+
+  $('#nav-link-contact').on('click', function() {
+    $('html, body').animate({
+      scrollTop: ($('#section-contact').offset().top - x)
+    }, 500); // 500 milliseconds
+  });
 });
