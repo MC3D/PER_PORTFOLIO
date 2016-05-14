f5544bbcab070fef257e5901eb10c1b22b0d142b
MC3D <m.chapman.home@gmail.com>
2016-05-13 23:13:07 -0400

    adds static timeline

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// $( document ).ready(
+//   $(window).on('scroll', function() {
+//     $('.timeline-event').each(function() {
+//       if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('timeline-icon').hasClass('is-hidden')) {
+//         $(this).find('.timeline-icon, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
+//       }
+//     });
+//   }));
