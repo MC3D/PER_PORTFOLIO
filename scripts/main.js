fa13b1dbd29a6c18301214e001d0681d4a9b585c
MC3D <m.chapman.home@gmail.com>
2016-05-15 23:38:30 -0400

    adds timeline animation

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ $(function() {
   var y = $('#nav').offset().top;
   console.log(y);
 
+  //on scolling, show/animate timeline events when enter the viewport
   $(window).on('scroll', function() {
     if (y <= $(window).scrollTop()) {
       // if so, add the fixed class
@@ -11,6 +12,12 @@ $(function() {
       // otherwise remove it
       $('#nav').removeClass('fixed');
     }
+
+    (!window.requestAnimationFrame) ? setTimeout(function() {
+      showEvents(timelineEvents, offset);
+    }, 100): window.requestAnimationFrame(function() {
+      showEvents(timelineEvents, offset);
+    });
   });
 
   $('#nav-link-skills').on('click', function() {
@@ -43,13 +50,36 @@ $(function() {
     }, 500); // 500 milliseconds
   });
 
-  $('.toggle-button').on('click', function(){
+  $('.toggle-button').on('click', function() {
     var display = $('.nav-list').css('display');
-    if(display === 'none'){
+    if (display === 'none') {
       $('.nav-list').css('display', 'flex');
     } else {
       $('.nav-list').css('display', 'none');
     }
 
   });
+
+  var timelineEvents = $('.timeline-event'),
+    offset = 0.8;
+
+  function hideEvents(events, offset) {
+    events.each(function() {
+      if ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) {
+        $(this).find('.timeline-icon, .timeline-content').addClass('is-hidden');
+      }
+    });
+  }
+
+  function showEvents(events, offset) {
+    events.each(function() {
+      if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.timeline-icon').hasClass('is-hidden')) {
+        $(this).find('.timeline-icon, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
+      }
+    });
+  }
+
+  //hide timeline events which are outside the viewport
+  hideEvents(timelineEvents, offset);
+
 });
