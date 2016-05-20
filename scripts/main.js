60b79dd59afccf0310dcab23ad4393dc1398039a
MC3D <m.chapman.home@gmail.com>
2016-05-19 21:16:17 -0400

    adds mobile animations

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ $(function() {
 
   $('.nav-link').on('click', function(event) {
     var text = 'section-' + (event.target.innerText).toLowerCase();
-    if($(window).width() >= 1170) {
+    if ($(window).width() >= 1170) {
       x = $('#nav').height();
     }
     $('.nav-list').css('display', 'none');
@@ -31,52 +31,25 @@ $(function() {
 
 
 
-  function checkAnimationElements(){
+  function checkAnimationElements() {
     var windowHeight = $window.height();
     var windowTopPosition = $window.scrollTop();
     var windowBottomPosition = (windowTopPosition + windowHeight);
 
     $.each($animationElements, function() {
-    var $element = $(this);
-    var elementHeight = $element.outerHeight();
-    var elementTopPosition = $element.offset().top;
-    var elementBottomPosition = (elementTopPosition + elementHeight);
+      var $element = $(this);
+      var elementHeight = $element.outerHeight();
+      var elementTopPosition = $element.offset().top;
+      var elementBottomPosition = (elementTopPosition + elementHeight);
 
-    if ((elementBottomPosition >= windowTopPosition) &&
+      if ((elementBottomPosition >= windowTopPosition) &&
         (elementTopPosition <= windowBottomPosition)) {
-      $element.find('.timeline-icon, .timeline-content').addClass('bounce-in');
-    } else {
-      $element.find('.timeline-icon, .timeline-content').removeClass('bounce-in');
-    }
-  });
-}
-
-
-
-
-
-
-  // var timelineEvents = $('.timeline-event'),
-  //   offset = 0.8;
-  //
-  // function hideEvents(events, offset) {
-  //   events.each(function() {
-  //     if ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) {
-  //       $(this).find('.timeline-icon, .timeline-content').addClass('is-hidden');
-  //     }
-  //   });
-  // }
-  //
-  // function showEvents(events, offset) {
-  //   events.each(function() {
-  //     if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.timeline-icon').hasClass('is-hidden')) {
-  //       $(this).find('.timeline-icon, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
-  //     }
-  //   });
-  // }
-
-  //hide timeline events which are outside the viewport
-  // hideEvents(timelineEvents, offset);
+        $element.find('.timeline-icon, .timeline-content').addClass('bounce-in');
+      } else {
+        $element.find('.timeline-icon, .timeline-content').removeClass('bounce-in');
+      }
+    });
+  }
 
   $(window).on('scroll resize', function() {
     if (y <= $(window).scrollTop()) {
@@ -89,15 +62,8 @@ $(function() {
 
     checkAnimationElements();
 
-    // window.requestAnimationFrame(showEvents);
-
-    // (!window.requestAnimationFrame) ? setTimeout(function() {
-    //   showEvents(timelineEvents, offset);
-    // }, 100): window.requestAnimationFrame(function() {
-    //   showEvents(timelineEvents, offset);
-    // });
   });
 
-$window.trigger('scroll');
+  $window.trigger('scroll');
 
 });
